66a5f878ba885cc4a929a1c1f558eda9647f605a
Bektur Ismailov <bismailov163@gmail.com>
2026-07-30 16:25:20 +0600

    task 1: fixing coroutine safe

--- app/service/dashboard.py
+++ app/service/dashboard.py
@@ -1,9 +1,11 @@
 import asyncio
 
 from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.database.db import engine
 from app.database.repository import EventRepo
 from app.exception.event import EventNotFound
-from app.schemas import EventDashboard
+from app.schemas import EventDashboard, OccupancyDashboard, SalesDashboard
 
 
 async def event_dashboard_service(event_id: int, organizer_id: int, db: AsyncSession) -> EventDashboard:
@@ -13,8 +15,8 @@ async def event_dashboard_service(event_id: int, organizer_id: int, db: AsyncSes
         raise EventNotFound(event_id)
 
     sales_dashboard, occupancy_dashboard = await asyncio.gather(
-        event_repo.get_sales_dashboard(event_id),
-        event_repo.get_occupancy_dashboard(event_id),
+        load_sales_dashboard(event_id),
+        load_occupancy_dashboard(event_id),
     )
 
     return EventDashboard(
@@ -23,3 +25,13 @@ async def event_dashboard_service(event_id: int, organizer_id: int, db: AsyncSes
         sales=sales_dashboard,
         occupancy=occupancy_dashboard,
     )
+
+
+async def load_sales_dashboard(event_id: int) -> SalesDashboard:
+    async with AsyncSession(engine, expire_on_commit=False) as db:
+        return await EventRepo(db).get_sales_dashboard(event_id)
+
+
+async def load_occupancy_dashboard(event_id: int) -> OccupancyDashboard:
+    async with AsyncSession(engine, expire_on_commit=False) as db:
+        return await EventRepo(db).get_occupancy_dashboard(event_id)
