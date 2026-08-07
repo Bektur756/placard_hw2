627704a7ff0eec04c0caab66a33b8aad9a15f3c0
Bektur Ismailov <bismailov163@gmail.com>
2026-08-07 21:59:09 +0600

    starting event tracker

---
+++ app/lifespan_tasks/event_view_tracker.py
@@ -0,0 +1,38 @@
+import asyncio
+
+
+class EventViewTracker:
+    def __init__(self):
+        self.queue = asyncio.Queue()
+
+    async def start(self):
+        self._worker_stask = asyncio.create_task(self._track_events())
+
+    async def stop(self):
+        self._worker_stask.cancel()
+
+        try:
+            await self._worker_stask
+        except:
+            pass
+
+    async def add_to_queue(self, event_id):
+        await self.queue.put(event_id)
+
+    async def _track_events(self):
+        events = []
+        while True:
+            try:
+                event = await asyncio.wait_for(self.queue.get(), timeout=5)
+                events.append(event)
+            except asyncio.TimeoutError:
+                if events:
+                    await self._insert_events_to_db(events)
+                    events = []
+
+            if len(events) > 10:
+                await self._insert_events_to_db(events)
+                events = []
+
+    async def _insert_events_to_db(self, events):
+        pass
--- app/service/event.py
+++ app/service/event.py
@@ -1,6 +1,8 @@
+import asyncio
 from redis.asyncio import Redis
 from app.database.db import DatabaseManager
 from app.exception.event import EventNotFound
+from app.lifespan_tasks import event_view_tracker
 from app.schemas import EventRead
 
 
@@ -8,11 +10,13 @@ class EventService:
     def __init__(self, db: DatabaseManager, redis: Redis) -> None:
         self.db = db
         self.redis = redis
+        self.event_view_tracker = event_view_tracker
 
-    async def get_event_by_id(self, event_id: int) -> EventRead:
+    async def get_event_by_id(self, event_id: int, client_host: str | None) -> EventRead:
         key = f"event_{event_id}"
         event = await self.redis.get(key)
         if event:
+            await self.add_to_event_counter(event_id, client_host)
             return EventRead.model_validate_json(event)
 
         async with self.redis.lock(
@@ -22,12 +26,19 @@ class EventService:
         ):
             event = await self.redis.get(key)
             if event:
+                await self.add_to_event_counter(event_id, client_host)
                 return EventRead.model_validate_json(event)
 
             event = await self.db.events.get_by_id(event_id)
             if not event:
                 raise EventNotFound(event_id)
 
+            await self.add_to_event_counter(event_id, client_host)
             event_read = EventRead.model_validate(event)
             await self.redis.set(key, event_read.model_dump_json(), ex=60)
             return event_read
+
+    async def add_to_event_counter(self, event_id: int, client_host: str | None) -> None:
+        is_unique = await self.redis.set(f"event_view:{client_host}:{event_id}", "1", nx=True, ex=300)
+        if is_unique and client_host:
+            await self.event_view_tracker.add_to_queue(event_id)
